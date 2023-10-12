196724db880c6b0d0c15c54c99dfc9e9426dde37
Nomerk <97950056+Nomerk@users.noreply.github.com>
2023-10-12 20:10:51 +0700

    Add files via upload

---
+++ prakalgo3.py
@@ -0,0 +1,25 @@
+bulan = int(input("Masukkan bulan (1-12): "))
+tahun = int(input("Masukkan tahun : "))
+
+hari_dalam_bulan = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
+    hari_dalam_bulan[2] = 29
+
+while bulan < 1 or bulan > 12:
+    bulan = int(input("Masukkan bulan (1-12): "))
+
+print("Jumlah hari dalam bulan", bulan, "tahun", tahun, "adalah", hari_dalam_bulan[bulan], "hari.")
